d19088415a317a7fbcae6a54008ba8a436ee1b8c
K1Dev <67011212055@msu.ac.th>
2025-09-03 22:19:58 +0700

    prototype

---
+++ src/Config.java
@@ -0,0 +1,13 @@
+public class Config {
+    public static int W = 900;
+    public static int H = 600;
+    public static int N = 12;
+    public static int R_MIN = 10;
+    public static int R_MAX = 18;
+    public static double V_MIN = 1.2;
+    public static double V_MAX = 3.2;
+    public static double SPEED_UP = 1.08;
+    public static double MAX_SPEED = 8.0;
+    public static int TICK_MS = 12;
+    public static int FPS_MS = 16;
+}
